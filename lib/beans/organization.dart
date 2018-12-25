862a819d79ee40d8263c2f6940c7f10d075e77fb
zsirfs <zhenglfsir@gmail.com>
2018-12-25 13:12:46 +0800

    [add] add the bean example

--- lib/beans/organization.dart
+++ lib/beans/organization.dart
@@ -1,3 +1,19 @@
+/*
+{
+    "login": "randAdvancer",
+    "id": 31266392,
+    "node_id": "xxx",
+    "url": "https://api.github.com/orgs/randAdvancer",
+    "repos_url": "https://api.github.com/orgs/randAdvancer/repos",
+    "events_url": "https://api.github.com/orgs/randAdvancer/events",
+    "hooks_url": "https://api.github.com/orgs/randAdvancer/hooks",
+    "issues_url": "https://api.github.com/orgs/randAdvancer/issues",
+    "members_url": "https://api.github.com/orgs/randAdvancer/members{/member}",
+    "public_members_url": "https://api.github.com/orgs/randAdvancer/public_members{/member}",
+    "avatar_url": "https://avatars1.githubusercontent.com/u/31266392?v=4",
+    "description": null
+}
+*/
 import 'package:json_annotation/json_annotation.dart';
 
 part 'organization.g.dart';
